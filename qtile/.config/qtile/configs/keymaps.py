c2a6df2fbdf451d8a214fb5fb8eed28f44e36c03
thatanjan <thatanjan@gmail.com>
2022-07-21 18:26:00 +0600

    group won't be switched when moving window

--- qtile/.config/qtile/configs/keymaps.py
+++ qtile/.config/qtile/configs/keymaps.py
@@ -1,5 +1,4 @@
 from libqtile.config import Group, Key
-from libqtile.config import Key
 from libqtile.lazy import lazy
 
 from .global_vars import mod, terminal, shift
@@ -88,15 +87,19 @@ for group in groups:
                 desc="Switch to group {}".format(group.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
+            # Key(
+            #     [mod, "shift"],
+            #     group.name,
+            #     lazy.window.togroup(group.name, switch_group=True),
+            #     desc="Switch to & move focused window to group {}".format(group.name),
+            # ),
+            # Or, use below if you prefer not to switch to that group.
+            # mod1 + shift + letter of group = move focused window to group
             Key(
                 [mod, "shift"],
                 group.name,
-                lazy.window.togroup(group.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(group.name),
+                lazy.window.togroup(group.name),
+                desc="move focused window to group {}".format(group.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
